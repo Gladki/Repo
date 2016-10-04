d3c8ceba521ab1ad9798d9fa17b42b4b2e1c0dea
Aleksander Gładysz <Gladki@MBPAleksander2.home>
2016-10-04 21:14:55 +0200

    new project

---
+++ DotnetCoreProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var val = new TestClass();
+            val.Val1 = 2;
+            val.Val2 = 3;
+
+            Console.Write($"aaa{0}",val.Val1);
+
+
+        }
+    }
+}
---
+++ DotnetCoreProject/src/Models/TestClass.cs
@@ -0,0 +1,7 @@
+public class TestClass {
+
+public int Val1 { get; set; }
+public int Val2 { get; set; }
+
+
+}
